f92f0450c128165345920b0e58fc99b54c2ed4af
DimaAntoniuk <dimaantoniuk09@gmail.com>
2020-05-04 22:08:02 +0300

    fix: vocabulary mistakes

--- Lab3/docker-mysql/db_lab_3.sql
+++ Lab3/docker-mysql/db_lab_3.sql
@@ -2,44 +2,44 @@ SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
-CREATE SCHEMA IF NOT EXISTS Antoniuk_db DEFAULT CHARACTER SET utf8 ;
-USE Antoniuk_db ;
+CREATE SCHEMA IF NOT EXISTS antoniuk_db DEFAULT CHARACTER SET utf8 ;
+USE antoniuk_db ;
 
-DROP TABLE IF EXISTS Antoniuk_db.`owner`;
-DROP TABLE IF EXISTS Antoniuk_db.station;
-DROP TABLE IF EXISTS Antoniuk_db.owner_has_station;
-DROP TABLE IF EXISTS Antoniuk_db.pannel;
-DROP TABLE IF EXISTS Antoniuk_db.pannel_data;
-DROP TABLE IF EXISTS Antoniuk_db.price;
-DROP TABLE IF EXISTS Antoniuk_db.output;
-DROP TABLE IF EXISTS Antoniuk_db.household;
-DROP TABLE IF EXISTS Antoniuk_db.station_has_household;
+DROP TABLE IF EXISTS antoniuk_db.`owner`;
+DROP TABLE IF EXISTS antoniuk_db.station;
+DROP TABLE IF EXISTS antoniuk_db.owner_has_station;
+DROP TABLE IF EXISTS antoniuk_db.panel;
+DROP TABLE IF EXISTS antoniuk_db.panel_data;
+DROP TABLE IF EXISTS antoniuk_db.price;
+DROP TABLE IF EXISTS antoniuk_db.output;
+DROP TABLE IF EXISTS antoniuk_db.household;
+DROP TABLE IF EXISTS antoniuk_db.station_has_household;
 
 
-CREATE TABLE Antoniuk_db.`owner` (
+CREATE TABLE antoniuk_db.`owner` (
   id INT NOT NULL AUTO_INCREMENT,
   first_name VARCHAR(45) NULL,
   last_name VARCHAR(45) NULL,
   PRIMARY KEY (id)
 )ENGINE = InnoDB;
 
-CREATE TABLE Antoniuk_db.station (
+CREATE TABLE antoniuk_db.station (
   id INT NOT NULL AUTO_INCREMENT,
-  number_of_pannels INT NULL,
+  number_of_panels INT NULL,
   address VARCHAR(45) NULL,
   time_of_usage INT NULL,
   PRIMARY KEY (id)
 )ENGINE = InnoDB;
 
 
-CREATE TABLE Antoniuk_db.household (
+CREATE TABLE antoniuk_db.household (
   id INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(45) NULL,
   PRIMARY KEY (id)
 )ENGINE = InnoDB;
 
 
-CREATE TABLE Antoniuk_db.output (
+CREATE TABLE antoniuk_db.output (
   id INT NOT NULL AUTO_INCREMENT,
   power_per_hour INT NULL,
   time_start DATETIME NULL,
@@ -50,30 +50,30 @@ CREATE TABLE Antoniuk_db.output (
 )ENGINE = InnoDB;
 
 
-CREATE TABLE Antoniuk_db.pannel (
+CREATE TABLE antoniuk_db.panel (
   id INT NOT NULL AUTO_INCREMENT,
   `type` VARCHAR(45) NULL,
-  carpacity INT NULL,
+  `capacity` INT NULL,
   station_id INT NOT NULL,
   PRIMARY KEY (id)
 )ENGINE = InnoDB;
 
 
-CREATE TABLE Antoniuk_db.owner_has_station (
+CREATE TABLE antoniuk_db.owner_has_station (
   owner_id INT NOT NULL,
   station_id INT NOT NULL,
   PRIMARY KEY (owner_id, station_id)
 )ENGINE = InnoDB;
 
 
-CREATE TABLE Antoniuk_db.station_has_household (
+CREATE TABLE antoniuk_db.station_has_household (
   station_id INT NOT NULL,
   household_id INT NOT NULL,
   PRIMARY KEY (station_id, household_id)
 )ENGINE = InnoDB;
 
 
-CREATE TABLE Antoniuk_db.price (
+CREATE TABLE antoniuk_db.price (
   id INT NOT NULL AUTO_INCREMENT,
   `value` INT NULL,
   time_of_price_begin TIME NULL,
@@ -82,14 +82,14 @@ CREATE TABLE Antoniuk_db.price (
 )ENGINE = InnoDB;
 
 
-CREATE TABLE Antoniuk_db.pannel_data (
+CREATE TABLE antoniuk_db.panel_data (
   id INT NOT NULL AUTO_INCREMENT,
   `date` DATETIME NULL,
   angel INT NULL,
   power INT NULL,
   charge_level INT NULL,
-  pannel_id INT NOT NULL,
-  PRIMARY KEY (id, pannel_id)
+  panel_id INT NOT NULL,
+  PRIMARY KEY (id, panel_id)
 )ENGINE = InnoDB;
 
 ALTER TABLE output
@@ -97,20 +97,20 @@ ALTER TABLE output
   ADD INDEX fk_output_price1_idx (price_id ASC) VISIBLE,
   ADD CONSTRAINT fk_output_station1
     FOREIGN KEY (station_id)
-    REFERENCES Antoniuk_db.station (id)
+    REFERENCES antoniuk_db.station (id)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   ADD CONSTRAINT fk_output_price1
     FOREIGN KEY (price_id)
-    REFERENCES Antoniuk_db.price (id)
+    REFERENCES antoniuk_db.price (id)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION;
 
-ALTER TABLE pannel
-	ADD INDEX fk_pannel_station1_idx (station_id ASC) VISIBLE,
-  ADD CONSTRAINT fk_pannel_station1
+ALTER TABLE panel
+	ADD INDEX fk_panel_station1_idx (station_id ASC) VISIBLE,
+  ADD CONSTRAINT fk_panel_station1
     FOREIGN KEY (station_id)
-    REFERENCES Antoniuk_db.station (id)
+    REFERENCES antoniuk_db.station (id)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION;
     
@@ -119,12 +119,12 @@ ALTER TABLE owner_has_station
   ADD INDEX fk_owner_has_station_owner_idx (owner_id ASC) VISIBLE,
   ADD CONSTRAINT fk_owner_has_station_owner
     FOREIGN KEY (owner_id)
-    REFERENCES Antoniuk_db.`owner` (id)
+    REFERENCES antoniuk_db.`owner` (id)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   ADD CONSTRAINT fk_owner_has_station_station1
     FOREIGN KEY (station_id)
-    REFERENCES Antoniuk_db.station (id)
+    REFERENCES antoniuk_db.station (id)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION;
 
@@ -133,20 +133,20 @@ ALTER TABLE station_has_household
   ADD INDEX fk_station_has_household_station1_idx (station_id ASC) VISIBLE,
   ADD CONSTRAINT fk_station_has_household_station1
     FOREIGN KEY (station_id)
-    REFERENCES Antoniuk_db.station (id)
+    REFERENCES antoniuk_db.station (id)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   ADD CONSTRAINT fk_station_has_household_household1
     FOREIGN KEY (household_id)
-    REFERENCES Antoniuk_db.household (id)
+    REFERENCES antoniuk_db.household (id)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION;
 
-ALTER TABLE pannel_data
-	ADD INDEX fk_pannel_data_pannel1_idx (pannel_id ASC) VISIBLE,
-  ADD CONSTRAINT fk_pannel_data_pannel1
-    FOREIGN KEY (pannel_id)
-    REFERENCES Antoniuk_db.pannel (id)
+ALTER TABLE panel_data
+	ADD INDEX fk_panel_data_panel1_idx (panel_id ASC) VISIBLE,
+  ADD CONSTRAINT fk_panel_data_panel1
+    FOREIGN KEY (panel_id)
+    REFERENCES antoniuk_db.panel (id)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION;
 
@@ -156,7 +156,7 @@ SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
 
 -- INSERTION
 START TRANSACTION;
-USE Antoniuk_db;
+USE antoniuk_db;
 
 INSERT INTO `owner` (first_name, last_name)
 VALUES
@@ -165,7 +165,7 @@ VALUES
     ("Nyasia ", "Hardin"), ("Darnell ", "Petersen"), ("Aubrey ", "Cortez");
 
 
-INSERT INTO station (number_of_pannels, address, time_of_usage)
+INSERT INTO station (number_of_panels, address, time_of_usage)
 VALUES
 	(1, "2594 Apple Lane", 10), ("2", "2982 Twin Willow Lane", 12), 
     (3, "1626 Pearcy Avenue", 12);
@@ -181,7 +181,7 @@ VALUES
 	(1, 1), (2, 2), (2, 3), (3, 3), (3, 4), (3, 5);
 
 
-INSERT INTO pannel (`type`, carpacity, station_id)
+INSERT INTO panel (`type`, `capacity`, station_id)
 VALUES
 	("LEAD ACID", 1000, 1), ("LEAD ACID", 1070, 2), ("LITHIUM", 2000, 2),
     ("NICAD", 500, 3), ("LITHIUM", 2300, 3), ("LEAD ACID", 1120, 3);
@@ -192,7 +192,7 @@ VALUES
 	(1, 2), (1, 3), (2, 3), (3, 3), (4, 3), (5, 2), (6, 2), (7, 1), (8, 2), (9, 3);
 
 
-INSERT INTO pannel_data (`date`, angel, power, charge_level, pannel_id)
+INSERT INTO panel_data (`date`, angel, power, charge_level, panel_id)
 VALUES
 	("2019-09-10 06:00:00", 60, 7, 10, 1), ("2019-09-10 09:00:00", 30, 7, 27, 1), ("2019-09-10 12:00:00", 10, 9, 100, 1),
     ("2019-09-10 15:00:00", -10, 8, 100, 1), ("2019-09-10 18:00:00", -30, 7, 100, 1), ("2019-09-10 21:00:00", -60, 7, 100, 1),
